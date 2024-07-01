bcea52dc8311c8ddffa649215f468b0a5ef42913
bvpav <78971592+bvpav@users.noreply.github.com>
2024-07-01 16:01:41 +0300

    added epilepsy

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,26 @@
 #include <GLFW/glfw3.h>
+#include <math.h>
+
+void hsv2rgb(float h, float s, float v, float *r, float *g, float *b) {
+	h = h / 360;
+	s = s / 100;
+	v = v / 100;
+	
+	int i = floor(h * 6);
+	float f = h * 6 - i;
+	float p = v * (1 - s);
+	float q = v * (1 - f * s);
+	float t = v * (1 - (1 - f) * s);
+	
+	switch (i % 6) {
+		case 0: *r = v, *g = t, *b = p; break;
+		case 1: *r = q, *g = v, *b = p; break;
+		case 2: *r = p, *g = v, *b = t; break;
+		case 3: *r = p, *g = q, *b = v; break;
+		case 4: *r = t, *g = p, *b = v; break;
+		case 5: *r = v, *g = p, *b = q; break;
+	}
+}
 
 int main(void)
 {
@@ -17,11 +39,16 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    float hue = 0;
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
-        glClearColor(1.f, 1.f, 0.f, 1.f);
+        hue = hue < 359 ? hue + 1 : 0;
+
+        float r, g, b;
+        hsv2rgb(hue, 50, 100, &r, &g, &b);
+        glClearColor(r, g, b, 1.f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
