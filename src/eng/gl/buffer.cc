c93d7d34c4f9072e88bea75a8a5d0811a069e0bc
bvpav <78971592+bvpav@users.noreply.github.com>
2024-07-06 20:56:09 +0300

    fixed attribute layout bug

--- src/eng/gl/buffer.cc
+++ src/eng/gl/buffer.cc
@@ -5,6 +5,7 @@
 namespace eng::gl {
 
 Buffer::Buffer(const eng::mesh::Vertex *vertices, size_t size)
+    : Buffer()
 {
     bind();
     glBufferData(GL_ARRAY_BUFFER, size, vertices, GL_STATIC_DRAW);
@@ -13,10 +14,12 @@ Buffer::Buffer(const eng::mesh::Vertex *vertices, size_t size)
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,
                           sizeof(eng::mesh::Vertex),
                           reinterpret_cast<void *>(offsetof(eng::mesh::Vertex, x)));
+    glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE,
                           sizeof (eng::mesh::Vertex),
                           reinterpret_cast<void *>(offsetof(eng::mesh::Vertex, norm_x)));
-    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE,
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE,
                           sizeof (eng::mesh::Vertex),
                           reinterpret_cast<void *>(offsetof(eng::mesh::Vertex, u)));
 }
